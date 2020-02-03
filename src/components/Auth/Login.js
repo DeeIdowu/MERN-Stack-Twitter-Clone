c65535e1fcb1912a4e07039b04047ad19863b3d6
Dennis Idowu <deethegreat@Denniss-MacBook-Air.local>
2020-02-03 18:40:54 -0500

    added components: home, auth-login & registration as well as material-ui icons and stylesheets

---
+++ src/components/Auth/Login.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+const Login = () => (
+  <div>
+    <h1>This is the Login Page</h1>
+  </div>
+);
+
+export default Login;
---
+++ src/components/Auth/Register.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+const Register = () => (
+  <div>
+    <h1>This is the Registration Page</h1>
+  </div>
+);
+
+export default Register;
