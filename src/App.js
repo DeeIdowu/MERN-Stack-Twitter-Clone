34ffde416998473dbc4e354dc437376abab353f2
Dennis Idowu <deethegreat@Denniss-MacBook-Air.local>
2020-02-03 21:24:07 -0500

    implemented header into app.js for all components to utilize

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "../src/components/Home/Home";
 import Login from "../src/components/Auth/Login";
 import Register from "../src/components/Auth/Register";
+import Main from "../src/components/Layout/Main/Main";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={Register} />
-        </Switch>
+        <Main>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+          </Switch>
+        </Main>
       </BrowserRouter>
     </div>
   );
