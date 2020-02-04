693b4c13718349f7a0a4c2afc748c82047f21d7e
Dennis Idowu <deethegreat@Denniss-MacBook-Air.local>
2020-02-04 17:56:53 -0500

    removal of store from actions folder and creation of authActions.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+
 import Home from "../src/components/Home/Home";
 import Login from "../src/components/Auth/Login";
 import Register from "../src/components/Auth/Register";
@@ -7,17 +10,19 @@ import Main from "../src/components/Layout/Main/Main";
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Main>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/register" component={Register} />
-          </Switch>
-        </Main>
-      </BrowserRouter>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <BrowserRouter>
+          <Main>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/register" component={Register} />
+            </Switch>
+          </Main>
+        </BrowserRouter>
+      </div>
+    </Provider>
   );
 }
 
--- client/src/reducers/errorReducer.js
+++ client/src/reducers/errorReducer.js
@@ -1,12 +1,11 @@
-import {GET_ERRORS} from '../constants/index';
-const initialState = {}
+import { GET_ERRORS } from "../constants/index";
+const initialState = {};
 
-
-export function (state = initialState, action){
-    switch(action.type){
-        case GET_ERRORS:
-            return action.payload
-        default:
-            return state
-    }
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case GET_ERRORS:
+      return action.payload;
+    default:
+      return state;
+  }
 }
