e8727baa80d46b05d8fe5e35c4971d00b5eb8346
Dennis Idowu <deethegreat@Denniss-MacBook-Air.local>
2020-02-12 12:25:34 -0500

    created Post components toand imported into Homepage

---
+++ client/src/components/Posts/ListPosts.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class ListPost extends Component {
+  render() {
+    return (
+      <div>
+        This is the ListPost Page
+        <h1>Listo</h1>
+      </div>
+    );
+  }
+}
+
+export default ListPost;
