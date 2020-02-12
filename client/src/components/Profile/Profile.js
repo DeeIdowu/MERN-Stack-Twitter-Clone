4466dc1682a182c2ba9f920ccfa9942bdc8ba317
Dennis Idowu <deethegreat@Denniss-MacBook-Air.local>
2020-02-12 18:13:04 -0500

    configured profile component set props and state and componentdidmount lifecycle for functionality

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,13 +1,47 @@
 import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import { connect } from "react-redux";
+import { getPostsByUserId, getUserProfile } from "../../actions/profileActions";
+import Post from "../Posts/Post";
+import LoadingPosts from "../Posts/LoadingPosts";
+
+const styles = {};
 
 class Profile extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.props.getPostsByUserId(this.props.match.params.userId);
+    this.props.getUserProfile(this.props.match.params.userId);
+  }
+
   render() {
-    return (
-      <div>
-        <h1>Profile page</h1>
-      </div>
-    );
+    const {
+      classes,
+      loadingPosts,
+      loadingProfile,
+      list,
+      auth,
+      user,
+      profile
+    } = this.props;
+
+    const items = list && list.map(el => <Post key={el._id} post={el} />);
+    return <div>{loadingPosts ? <LoadingPosts /> : items}</div>;
   }
 }
 
-export default Profile;
+const mapStateToProps = state => ({
+  loadingPosts: state.post.loading,
+  list: state.post.list,
+  profile: state.profile.user,
+  loadingProfile: state.profile.loading,
+  auth: state.auth,
+  user: state.auth.user
+});
+
+export default connect(mapStateToProps, { getPostsByUserId, getUserProfile })(
+  withStyles(styles)(Profile)
+);
