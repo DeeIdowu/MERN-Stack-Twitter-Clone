89ab46e01834d5a2ef804fc7286216d1c2f0a315
Dennis Idowu <deethegreat@Denniss-MacBook-Air.local>
2020-02-12 16:16:01 -0500

    styled Post component

--- client/src/components/Posts/Post.js
+++ client/src/components/Posts/Post.js
@@ -1,13 +1,51 @@
 import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import { Link } from "react-router-dom";
+
+const styles = {
+  paper: {
+    padding: 10,
+    display: "flex",
+    marginTop: 10
+  },
+  avatar: {
+    minWidth: 10,
+    margin: "4px 10px 4px 4px"
+  },
+  username: {
+    marginBottom: 5
+  },
+  time: {
+    marginLeft: 10,
+    color: "#bbb",
+    fontSize: 14
+  }
+};
 
 class Post extends Component {
   render() {
+    const { classes, post } = this.props;
     return (
-      <div>
-        <h1>Here is your Post</h1>
-      </div>
+      <Paper className={classes.paper}>
+        <div
+          className={classes.avatar}
+          style={{
+            backgroundColor: "#fffbfb"
+          }}
+        />
+        <div>
+          <h3 className={classes.login}>
+            <Link to="/">{post.user.username}</Link>
+            <span className={classes.time}>
+              {new Date(post.createdAt).toLocaleString()}
+            </span>
+          </h3>
+          {post.text}
+        </div>
+      </Paper>
     );
   }
 }
 
-export default Post;
+export default withStyles(styles)(Post);
